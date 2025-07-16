21f851a4f7158bd6c855e4ca586e66f3ef431451
RAJENDRAN ESHWARAN <10699484+RajendranEshwaran@users.noreply.github.com>
2025-07-16 11:18:53 -0400

    Initial Commit

---
+++ MacCalculator/MacCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacCalculatorApp.swift
+//  MacCalculator
+//
+//  Created by RajayGoms on 7/16/25.
+//
+
+import SwiftUI
+
+@main
+struct MacCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
